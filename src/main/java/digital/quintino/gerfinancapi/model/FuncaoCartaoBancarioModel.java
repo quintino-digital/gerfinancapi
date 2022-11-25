9c3afdea3eebc50dad120729cabea016a64f7caf
Desenvolvimento <repository.desenvolvimento@gmail.com>
2022-11-25 17:08:52 -0300

    :large_blue_circle: feature: Modelar TB_FUNCAO_CARTAO_BANCARIO

---
+++ src/main/java/digital/quintino/gerfinancapi/model/FuncaoCartaoBancarioModel.java
@@ -0,0 +1,27 @@
+package digital.quintino.gerfinancapi.model;
+
+public class FuncaoCartaoBancarioModel {
+
+    private Long codigo;
+
+    private String descricao;
+
+    public FuncaoCartaoBancarioModel() { }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
