ae60264e4060c92bdd6693a77600be3c3032e1a0
Desenvolvimento <repository.desenvolvimento@gmail.com>
2022-12-01 19:15:30 -0300

    :large_blue_circle: feature: Modelar TB_FUNCAO_CARTAO_BANCARIO

--- src/main/resources/db/migration/V001__create_table_tb-funcao-cartao-bancario.sql
+++ src/main/resources/db/migration/V001__create_table_tb-funcao-cartao-bancario.sql
@@ -1,5 +1,8 @@
 create table if not exists tb_funcao_cartao_bancario (
-    codigo integer auto_increment not null,
-    descricao varchar(100) not null unique,
-    primary key (codigo)
+    codigo integer not null,
+    descricao varchar(100) not null unique
 );
+
+alter table tb_funcao_cartao_bancario add constraint pk_funcao_cartao_bancario primary key (codigo);
+
+alter table tb_funcao_cartao_bancario add unique (descricao);
